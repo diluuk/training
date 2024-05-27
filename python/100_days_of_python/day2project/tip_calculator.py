e56d7efbe22c100b1ea3746ffc782553cbf5b266
diluuk <5418792+diluuk@users.noreply.github.com>
2024-05-27 12:50:14 -0500

    Update tip_calculator.py
    updated for readability

--- python/100_days_of_python/day2project/tip_calculator.py
+++ python/100_days_of_python/day2project/tip_calculator.py
@@ -1,22 +1,11 @@
-#If the bill was $150.00, split between 5 people, with 12% tip. 
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? "))
+tip_amount = int(input("How much tip would you like to give? 10, 12, or 15? "))
+split = int(input("How many people will split the bill? "))
 
-#Each person should pay (150.00 / 5) * 1.12 = 33.6
-#Format the result to 2 decimal places = 33.60
+tip_percentage = tip_amount / 100
 
-#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+grand_total = bill + (bill * tip_percentage)
+amount_paid_per_person = grand_total / split
 
-#Write your code below this line 👇
-
-print("Welcome to the tip calculator! ")
-total_bill = input("What was the total bill? ")
-tip_percent = input("How much tip would you like to give? 10, 12, or 15? ")
-num_people = input("How many people to split the bill? ")
-
-tip_percent_float = float(tip_percent) * .01
-new_tip = 1 + tip_percent_float
-
-split_amount = (int(total_bill) / int(num_people)) * float(new_tip)
-
-#final_amount = round(float(split_amount), 4)
-
-print(f"Each person should pay: ${split_amount:.2f}")
+print(f"{amount_paid_per_person:.2f}")
